b6e2f25c8a365a5439ff84b8246f1c41496e6d65
amackillop <austin.mackillop@gmail.com>
2019-12-06 14:26:15 -0500

    [AM NN] Packaging foundations-auth-proxy

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='foundations-auth-proxy',
+      version='0.0.1',
+      description='Foundations Authentication Proxy',
+      packages=find_packages(exclude=('tests', 'docs')),
+      requires=['Flask-Cors==3.0.6', 'python-dotenv==0.10.3', 'PyYAML==5.1.2'],
+      zip_safe=False)
