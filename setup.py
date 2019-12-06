4a57311a4de96cdffb3553f3638a6e2dc8e8b6a6
Nekhil Nagia <nekhilnagia16@gmail.com>
2019-12-06 15:59:33 -0500

    [AM NN] minior

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='foundations-auth-proxy',
       version='0.0.1',
       description='Foundations Authentication Proxy',
       packages=find_packages(exclude=('tests', 'docs')),
-      requires=['Flask-Cors==3.0.6', 'python-dotenv==0.10.3', 'PyYAML==5.1.2'],
+      install_requires=['Flask-Cors==3.0.6', 'python-dotenv==0.10.3', 'PyYAML==5.1.2'],
       zip_safe=False)
