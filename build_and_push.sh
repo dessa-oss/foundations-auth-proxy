11432ab0acbb8773754433e829b1c93ef7a8b312
calvinchoi <c.choi@dessa.com>
2019-12-12 15:19:10 -0500

    [CALVIN CZ] Add missing character

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,7 +4,7 @@ BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
 REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "TEAM" ]] && REGISTRY=$NEXUS_DOCKER_STAGING
 DOCKER_FILE="Dockerfile" && [[ $1 -eq "TEAM" ]] && DOCKER_FILE="Dockerfile-CE"
 
-docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE \
+docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
   && docker tag "$REGISTRY/auth-proxy:$BUILD_VERSION" "$REGISTRY/auth-proxy:latest" \
   && docker push "$REGISTRY/auth-proxy" \
   && echo "Successfully build and pushed auth-proxy to the $REGISTRY repository"
