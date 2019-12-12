a0e570fc7fea5b4caab2bce3ba8fa3c9ac4ccc55
calvinchoi <c.choi@dessa.com>
2019-12-12 15:21:12 -0500

    [CALVIN CZ] Typo

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
-REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "TEAM" ]] && REGISTRY=$NEXUS_DOCKER_STAGING
-DOCKER_FILE="Dockerfile" && [[ $1 -eq "TEAM" ]] && DOCKER_FILE="Dockerfile-CE"
+REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "CE" ]] && REGISTRY=$NEXUS_DOCKER_STAGING
+DOCKER_FILE="Dockerfile" && [[ $1 -eq "CE" ]] && DOCKER_FILE="Dockerfile-CE"
 
 docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
   && docker tag "$REGISTRY/auth-proxy:$BUILD_VERSION" "$REGISTRY/auth-proxy:latest" \
