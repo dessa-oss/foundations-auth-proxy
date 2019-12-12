ab312d65911b46a9b885e295cad518e0ed2bc89a
calvinchoi <c.choi@dessa.com>
2019-12-12 15:16:00 -0500

    [CALVIN CZ] Potentially fix syntax error

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
-REGISTRY=$(( $1 == "TEAM" ? $NEXUS_DOCKER_REGISTRY : $NEXUS_DOCKER_STAGING ))
+REGISTRY=$(( $1 -eq "TEAM" ? $NEXUS_DOCKER_REGISTRY : $NEXUS_DOCKER_STAGING ))
 
-DOCKER_FILE=$(( $1 == "TEAM" ? Dockerfile : Dockerfile-CE ))
+DOCKER_FILE=$(( $1 -eq "TEAM" ? Dockerfile : Dockerfile-CE ))
 
 docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE \
   && docker tag "$REGISTRY/auth-proxy:$BUILD_VERSION" "$REGISTRY/auth-proxy:latest" \
