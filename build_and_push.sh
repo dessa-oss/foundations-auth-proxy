2dd684616b5bdd3ae734c2eee18081d0d2f20879
calvinchoi <c.choi@dessa.com>
2019-12-12 15:18:29 -0500

    [CALVIN CZ] Try again to fix syntax

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
-REGISTRY=$(( $1 -eq "TEAM" ? $NEXUS_DOCKER_REGISTRY : $NEXUS_DOCKER_STAGING ))
-
-DOCKER_FILE=$(( $1 -eq "TEAM" ? Dockerfile : Dockerfile-CE ))
+REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "TEAM" ]] && REGISTRY=$NEXUS_DOCKER_STAGING
+DOCKER_FILE="Dockerfile" && [[ $1 -eq "TEAM" ]] && DOCKER_FILE="Dockerfile-CE"
 
 docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE \
   && docker tag "$REGISTRY/auth-proxy:$BUILD_VERSION" "$REGISTRY/auth-proxy:latest" \
