739426fcd007673ab5106a8dc98b28a2afb7b883
Kyle De Freitas <k.defreitas@dessa.com>
2019-12-13 09:12:37 -0500

    [KD] changed for domain to be consistent between ce and non-ce

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
-REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "CE" ]] && REGISTRY=${NEXUS_DOCKER_STAGING}/atlas-ce-dev
+REGISTRY=$NEXUS_DOCKER_REGISTRY && [[ $1 -eq "CE" ]] && REGISTRY=${NEXUS_DOCKER_REGISTRY}/atlas-ce-dev
 DOCKER_FILE="Dockerfile" && [[ $1 -eq "CE" ]] && DOCKER_FILE="Dockerfile-CE"
 
 docker build --network=host -t "$REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
