b126790540e9e130124aae4b59baa04e220cab8a
calvinchoi <c.choi@dessa.com>
2019-12-18 14:35:57 -0500

    [CALVIN] Debugging jenkins

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
+echo $1
 DOCKER_FILE="Dockerfile" && [[ $1 -eq "null" ]] && DOCKER_FILE="Dockerfile-null"
 
 docker build --network=host -t "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
