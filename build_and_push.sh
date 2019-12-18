3f93758e157a1e43ddb08b65c976955bb3381a9f
calvinchoi <c.choi@dessa.com>
2019-12-18 14:30:49 -0500

    [CALVIN] Fix syntax?

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
-DOCKER_FILE="Dockerfile" && [[ ${1:""} -eq "null" ]] && DOCKER_FILE="Dockerfile-null"
+DOCKER_FILE="Dockerfile" && [[ $1 -eq "null" ]] && DOCKER_FILE="Dockerfile-null"
 
 docker build --network=host -t "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
   && docker tag "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" "$NEXUS_DOCKER_REGISTRY/auth-proxy:latest" \
