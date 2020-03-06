f98f4278852220f0ce981f090ce83549b637c33b
Kyle De Freitas <kdefreitas@squareup.com>
2020-03-06 10:42:23 -0500

    [KD] changed the format for the build file

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,7 +3,15 @@
 BUILD_VERSION=`python get_version.py | sed 's/+/_/g'`
 DOCKER_FILE="Dockerfile" && [[ $1 == "null" ]] && DOCKER_FILE="Dockerfile-null"
 
-docker build --network=host -t "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE . \
-  && docker tag "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" "$NEXUS_DOCKER_REGISTRY/auth-proxy:latest" \
-  && docker push "$NEXUS_DOCKER_REGISTRY/auth-proxy" \
+docker build \
+  --network=host \
+  -t "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" -f $DOCKER_FILE \
+  . \
+  && \
+  
+docker tag \
+  "$NEXUS_DOCKER_REGISTRY/auth-proxy:$BUILD_VERSION" "$NEXUS_DOCKER_REGISTRY/auth-proxy:latest" \
+  
+&& docker push \
+  "$NEXUS_DOCKER_REGISTRY/auth-proxy" \
   && echo "Successfully build and pushed auth-proxy to the $NEXUS_DOCKER_REGISTRY repository"
